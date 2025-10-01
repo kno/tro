3e25b69a11340001cf6eb1e4b7579e1053e313ef
kNo linsms <linsms@gmail.com>
2025-10-01 20:57:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3671656834-7f31c",
+  "appId": "1:1041675655579:web:a6062c0a419bdf1ea5293e",
+  "apiKey": "AIzaSyBuCTzllzwzvyu8TYMsPDLGNjvHm4nxj2Y",
+  "authDomain": "studio-3671656834-7f31c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1041675655579"
+};
